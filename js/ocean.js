57496f1baa2674ea5ea2674a6e2e1473a3d2bedf
AranceLiu <fish70309@gmail.com>
2023-11-16 23:55:04 +0800

    Site updated: 2023-11-16 23:55:04

--- js/ocean.js
+++ js/ocean.js
@@ -102,7 +102,7 @@
           '<a href="https://m.facebook.com/sharer.php?u=' +
             encodedUrl +
             '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-          '<a href="http://line.me/R/msg/text/?' +
+          '<a href="line://msg/text/' +
             encodedUrl +
             '" class="article-share-lineapp" target="_blank" title="Lineapp"></a>',
           '<a href="https://plus.google.com/share?url=' +
